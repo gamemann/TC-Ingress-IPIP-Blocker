7dc95d0820fd2e5ede557005fd985ea9cac603ef
Christian Deacon <gamemann@gflclan.com>
2020-07-22 03:07:07 +0000

    Added headers.

---
+++ src/common.h
@@ -0,0 +1,32 @@
+#ifdef __BPF__
+#define SEC(NAME) __attribute__((section(NAME), used))
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define htons(x) ((__be16)___constant_swab16((x)))
+#define ntohs(x) ((__be16)___constant_swab16((x)))
+#define htonl(x) ((__be32)___constant_swab32((x)))
+#define ntohl(x) ((__be32)___constant_swab32((x)))
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define htons(x) (x)
+#define ntohs(X) (x)
+#define htonl(x) (x)
+#define ntohl(x) (x)
+#endif
+
+#ifdef DEBUG
+#ifndef printk
+# define printk(fmt, ...)                                      \
+    ({                                                         \
+        char ____fmt[] = fmt;                                  \
+        trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
+    })
+#endif
+#endif
+
+#ifndef offsetof
+# define offsetof(TYPE, MEMBER)	((uint16_t)&((TYPE *)0)->MEMBER)
+#endif
+#endif
+
+#define BASEDIR_MAPS "/sys/fs/bpf/tc/globals"
